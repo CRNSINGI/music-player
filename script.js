470b5404f9799483c4dcf3638a9f1bb348fc6ba3
Cesar <cesarnsingi@hotmail.com>
2024-10-15 16:30:43 +0400

    fix music play button

--- script.js
+++ script.js
@@ -31,4 +31,6 @@ if(song.play()){
 progress.onchange = function(){
     song.play();
     song.currentTime = progress.value;
+    ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
 }
